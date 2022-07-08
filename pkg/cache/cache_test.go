93401678a86c0271faf3a6c49d7e19d8a11fecfd
Denis <76144861+dantedenis@users.noreply.github.com>
2022-07-08 14:28:33 +0300

    Update cache_test.go

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -3,17 +3,42 @@ package cache
 import (
 	"testing"
 	"wb_test/pkg/model"
-	_"fmt"
+	"sync"
+	"math/rand"
+	"time"
 )
 
-// run with -race
 
 func TestCache(t *testing.T) {
 	c := NewCache()
+	wg := sync.WaitGroup{}
 	
-	for i:=0; i < 5; i++ {
-		go c.Add(model.NewUser(100))
+	for i:=0; i < 10; i++ {
+		wg.Add(1)
+		go func(){
+			wg.Done()
+			c.Add(model.NewUser("test", 1000))
+		}()
 	}
 	
+	wg.Wait()
 	
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			c.Update(c.GetUser(rand.Int()%10+1), c.GetUser(rand.Int()%10+1), 100)
+		}()
+	}
+	wg.Wait()
+	
+	var sum int
+	
+	for i:=1; i < 11; i++ {
+		sum += c.GetUser(i).GetBalance()
+	}
+	
+	if sum != 10000 {
+		t.Error("Excpected 10000, actual:", sum)
+	}
 }
